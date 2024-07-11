348ef60086598999de627956efc4f1c035672fd7
Allen Shaw <allen@JoineryHQ.com>
2024-07-11 16:43:54 -0500

    Initial commit.

---
+++ permlog.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://joineryhq.com
+ * @since             1.0.0
+ * @package           Permlog
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Permissions Logger
+ * Plugin URI:        https://joineryhq.com
+ * Description:       Log all changes to WordPress role permissions
+ * Version:           1.0.0
+ * Author:            Allen Shaw
+ * Author URI:        https://joineryhq.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       permlog
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-permlog.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_permlog() {
+
+	$plugin = new Permlog();
+	$plugin->run();
+
+}
+run_permlog();
